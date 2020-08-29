22c02719979a668862f6f09aad291e1e34a03b2e
ash <uetani.0411@gmail.com>
2020-08-29 15:57:21 +0900

    yesNoAPI

---
+++ section5/main.js
@@ -0,0 +1,36 @@
+new Vue({
+    el: "#app",
+    data: {
+        count: 0
+    },
+    methods: {
+        increment: function() {
+            this.count += 1
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+// 繰り返し
+// data: {
+//     list: ["リッご", "kkkk", "ddddd", "aaaa"]
+// }
+
+
+
+
+// イベントの利用
+// methods: {
+//     handleClick: function(event){
+//         alert(event.alert)
+//     }
+// }
