5c35c2066376ab3a8c5636bb6f108f60dbbc4d4b
ash <uetani.0411@gmail.com>
2020-09-01 16:16:25 +0900

    イベントハンドラresize

--- tsubo/vue.js
+++ tsubo/vue.js
@@ -5,19 +5,20 @@ Vue.filter('number_format', function(val) {
 var app = new Vue({
     el: '#app',
     data: {
-        width: window.innerWidth,
-        height: window.innerHeight
+        point: {
+            x: 0, y: 0
+        }
     },
     created: function(){
-        addEventListener("resize", this.resizeHandler);
+        addEventListener("mousemove", this.mousemoveHandler);
     },
     beforeDestroy: function() {
-        removeEventListener("resize", this.resizeHandler);
+        removeEventListener("mousemove", this.mousemoveHandler);
     },
     methods: {
-        resizeHandler: function() {
-            this.width = $event.target.innerWidth;
-            this.height = $event.target.innerHeight;
+        mousemoveHandler: function($event) {
+            this.point.x = $event.clientX
+            this.point.y = $event.clientY
         }
     }
 });
