a5b8e62efcf8633d100a796c3db7a09fb090e87c
ash <uetani.0411@gmail.com>
2020-09-01 14:58:31 +0900

    算出プロパティ

--- tsubo/vue.js
+++ tsubo/vue.js
@@ -5,14 +5,15 @@ Vue.filter('number_format', function(val) {
 var app = new Vue({
     el: '#app',
     data: {
-        price: 1280
+        year: (new Date()).getFullYear()
     },
-    filters: {
-        number_format: function(val) {
-            return val.toLocaleString();
-        },
-        unit: function(val) {
-            return val + "円";
+    computed: {
+        isUrudoshi: function() {
+            if((this.year % 4 == 0) && (this.year % 100 != 0) || (this.year % 400 == 0)) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
 });
