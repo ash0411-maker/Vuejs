b002a28e2fc1f136fbc3838e7bbdaea162bd9f40
ash <uetani.0411@gmail.com>
2020-09-02 22:06:18 +0900

    JSON NULLエラー

--- shohinPage/main.js
+++ shohinPage/main.js
@@ -57,17 +57,22 @@
 // });
 
 
-
-var btnLoad = document.querySelector("#load");
-btnLoad.addEventListener("click", function(event) {
-  var xmlHttpRequest = new XMLHttpRequest();
+var btnLoad = document.querySelector('#load');
+// 読み込みボタンのクリックイベントハンドラを定義
+btnLoad.addEventListener('click', function(event) {
+  //【手順1】XMLHttpRequestオブジェクトのインスタンスを生成
+  var xmlHttpRequest  = new XMLHttpRequest();
+  //【手順2】通信状態の変化を監視するイベントハンドラを設定
   xmlHttpRequest.onreadystatechange = function() {
-    if(this.readyState == 4) {
+    // レスポンスの受信が正常に完了したとき
+    if (this.readyState == 4) {
       console.log(this.readyState, this.response);
     }
-  }
-  xmlHttpRequest.responseType = "json";
-  xmlHttpRequest.open("GET", "products.json");
+  };
+  //【手順3】レスポンスの形式を指定する
+  xmlHttpRequest.responseType = 'json';
+  //【手順4】リクエストメソッドと読み込むファイルのパスを指定する
+  xmlHttpRequest.open('GET', 'products.json');
+  //【手順5】リクエストを送信する（非同期通信を開始する）
   xmlHttpRequest.send();
 });
-
