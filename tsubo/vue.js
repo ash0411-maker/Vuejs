2f86ddd3d6dc306043ccc31899a985790adfc796
ash <uetani.0411@gmail.com>
2020-09-02 10:35:07 +0900

    selectboxの内容を動的に生成

--- tsubo/vue.js
+++ tsubo/vue.js
@@ -5,11 +5,12 @@ Vue.filter('number_format', function(val) {
 var app = new Vue({
     el: '#app',
     data: {
-        category: []
+        answer: "選択しください",
+        options: [
+            {code: "ans1", label: "初めて"},
+            {code: "ans2", label: "週１"},
+            {code: "ans3", label: "月１"},
+            {code: "ans4", label: "半年"},
+        ]
     },
-    computed: {
-        selectedCategory: function() {
-            return this.category.length >= 1 ? this.category.join() : "" ;
-        }
-    }
 });
