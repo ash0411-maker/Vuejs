2c6340b57a750ca1bcaafefd633eda1e3f8c485d
ash <uetani.0411@gmail.com>
2020-09-02 09:14:31 +0900

    全角入力反映

--- tsubo/vue.js
+++ tsubo/vue.js
@@ -5,27 +5,11 @@ Vue.filter('number_format', function(val) {
 var app = new Vue({
     el: '#app',
     data: {
-        stock: 10,
-        message: ""
+        year: (new Date()).getFullYear()
     },
     methods: {
-        onDeleteItem: function() {
-            this.stock--;
-        }
-    },
-    // watch: {
-    //     stock: function(newStock, oldStock) {
-    //         if(newStock == 0) {
-    //             this.message = "売り切れ";
-    //         }
-    //     }
-    // },
-    computed: {
-        statusMessage: function() {
-            if(this.stock <= 0) {
-                return "売り切れ";
-            }
-            return "";
+        yearInputHolder: function($event) {
+            this.year = $event.target.value;
         }
     }
 });
