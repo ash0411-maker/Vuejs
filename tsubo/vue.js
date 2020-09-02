dba044da67dc200da00be32116da2873f2ead708
ash <uetani.0411@gmail.com>
2020-09-02 11:00:54 +0900

    カレンダー

--- tsubo/vue.js
+++ tsubo/vue.js
@@ -5,12 +5,24 @@ Vue.filter('number_format', function(val) {
 var app = new Vue({
     el: '#app',
     data: {
-        answer: "選択しください",
-        options: [
-            {code: "ans1", label: "初めて"},
-            {code: "ans2", label: "週１"},
-            {code: "ans3", label: "月１"},
-            {code: "ans4", label: "半年"},
-        ]
+        arrival_date: null,
+        min_date: null
     },
+    created: function() {
+        // 何も指定されなかった場合は現在の（デバイスに）設定されている時刻を取得する。
+        var dt = new Date();
+        dt.setDate(dt.getDate() + 1);
+        this.arrival_date = this.formatDate(dt);
+        this.min_date = this.arrival_date;
+    },
+    methods: {
+        formatDate: function(dt) {
+            var y = dt.getFullYear();
+            var m = ("00" + (dt.getMonth() + 1)).slice(-2);
+            var d = ("00" + dt.getDate()).slice(-2);
+            console.log(d);
+            var result = y + "-" + m + "-" + d;
+            return result;
+        }
+    }
 });
