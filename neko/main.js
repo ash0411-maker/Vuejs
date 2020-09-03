25a94011accf83a3e278083699fb88ab3f0c4421
ash <uetani.0411@gmail.com>
2020-09-03 16:37:29 +0900

    モンスターへの攻撃

--- neko/main.js
+++ neko/main.js
@@ -3,8 +3,8 @@ var app = new Vue({
     data: {
         name: "",
         list: [
-            {id: 1, name: "スライム", hp: "10"},
-            {id: 2, name: "ゴーレム", hp: "30"},
+            {id: 1, name: "スライム", hp: "60"},
+            {id: 2, name: "ゴーレム", hp: "100"},
             {id: 3, name: "キラーマシン", hp: "100"},
         ]
     },
@@ -13,12 +13,14 @@ var app = new Vue({
             var max = this.list.reduce(function(a, b){
                 return a > b.id? a : b.id
             }, 0)
-
             this.list.push({
                 id: max += 1,
                 name: this.name,
                 hp: 500
             })
+        },
+        attack: function(index) {
+            this.list[index].hp -= 10
         },
         input: function($event) {
             this.name = $event.target.value
