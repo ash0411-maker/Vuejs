947a248b9f67496c3c4933c7fe31a43cbb6c1649
ash <uetani.0411@gmail.com>
2020-09-09 13:16:02 +0900

    テンプレート初成功

--- tsubo/component/show-hello.js
+++ tsubo/component/show-hello.js
@@ -1,9 +1,15 @@
-Vue.component('mt-product', {
+Vue.component('my-product', {
     template: `
-    <div>
-        <span>{{name}}</span>:<span>{{}price}</span>
-    </div>
-    `,
-    // propに定義したプロパティは親からデータを受け取るための入れ物
-    props: ["name", "price"]
+        <div>
+            <span>{{name}}</span>:<span>{{price}}</span>
+        </div>
+        `,
+    data: function() {
+        return {
+            name: 'スマホケース',
+            price: 980
+        }
+    }
+    // // propに定義したプロパティは親からデータを受け取るための入れ物
+    // props: ["name", "price"]
 });
