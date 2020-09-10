577f2b37273137bad5cc71883c0cf647dbbddb7b
ash <uetani.0411@gmail.com>
2020-09-10 15:08:01 +0900

    算出プロパティ

--- neko/main.js
+++ neko/main.js
@@ -1,9 +1,25 @@
-var scroll = new SmoothScroll()
 new Vue({
     el: '#app',
-    methods: {
-        scrollTop: function () {
-        scroll.animateScroll(0)
+    data: {
+        budget: 300,
+        limit: 2,
+        list: [
+            {id: 1, name: "りんご", price: 100},
+            {id: 2, name: "ばなな", price: 200},
+            {id: 3, name: "イチゴ", price: 400},
+            {id: 4, name: "オレンジ", price: 300},
+            {id: 5, name: "メロン", price: 500}
+        ]
+    },
+    computed: {
+        matched: function() {
+            // budget以下のリストを返す
+            return this.list.filter(function(el) {
+                return el.price <= this.budget
+            })
+        },
+        limited: function() {
+            return this.matched.slice(0, this.limit)
         }
     }
 })
