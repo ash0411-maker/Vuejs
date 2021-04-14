5e852f58d8757c5f095eacb64314dc55b6e387e1
ash <satoshi.0411@icloud.com>
2021-04-14 20:55:24 +0900

    template

--- Udemy/section4/template/main.js
+++ Udemy/section4/template/main.js
@@ -4,16 +4,6 @@ var app = new Vue({
       message: 'インスタンス2',
       name: 'ashketcham'
   },
-  methods: {
-    changeMessage: function() {
-      app.message = "変えてやった"
-    }
-  },
-  computed: {
-    myData: function() {
-      return this.$data
-    }
-  },
   template: '<p>{{ message }}</p>'
 })
 
