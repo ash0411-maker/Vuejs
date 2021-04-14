06de368bc5bb6c21cdc89837d9668db3a3f33673
ash <satoshi.0411@icloud.com>
2021-04-14 20:23:25 +0900

    access form outside of instance

---
+++ Udemy/section4/外側からVueインスタンスにアクセスする/main.js
@@ -0,0 +1,15 @@
+var app1 = new Vue({
+    el: '#app1',
+    data: {
+        message: 'インスタンス1'
+    }
+})
+
+app1.message = "書き換えた！"
+
+var app2 = new Vue({
+    el: '#app2',
+    data: {
+        message: 'インスタンス2'
+    }
+})
