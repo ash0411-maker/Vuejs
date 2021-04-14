d88fe66723504e4723ac2166bc26d15e7d252d3e
ash <satoshi.0411@icloud.com>
2021-04-14 20:44:38 +0900

    get instance data

---
+++ Udemy/section4/$data/main.js
@@ -0,0 +1,15 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+      message: 'インスタンス2',
+      name: 'ashketcham'
+  },
+  methods: {
+    changeMessage: function() {
+      app.message = "変えてやった"
+    }
+  }
+})
+
+// $dataでインスタンスのデータを一覧で取得できる
+console.log(app.$data);
