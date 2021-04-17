2ef61e8f51a46c8e18ad480db6ee423371448a5c
ash <satoshi.0411@icloud.com>
2021-04-18 08:13:29 +0900

    slot html毎データを渡す

---
+++ Udemy/section8/App_vue/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <LikeHeaderLocal>
+      <h1>トータルのいいね数<br>親から子へHTMLごとデータを渡す（slot）</h1>
+      <h2>いいね（{{ number }}）</h2>
+    </LikeHeaderLocal>
+
+    <LikeNumberGlobal :props-number="number" v-on:emit-number="number = $event"></LikeNumberGlobal>
+    <LikeNumberGlobal v-on:emit-number="number = $event"></LikeNumberGlobal>
+  </div>
+</template>
+
+<script>
+import LikeHeaderLocal from './components/LikeHeader.vue'
+
+export default {
+  data() {
+    return {
+      number: 10
+    }
+  },
+  components: {
+    LikeHeaderLocal
+  }
+}
+</script>
---
+++ Udemy/section8/App_vue/src/components/LikeHeader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="LikeHeaderLocal">
+    <div>
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["headerText"],
+}
+</script>
+
+
+<style scoped>
+#LikeHeaderLocal {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  border: solid;
+  margin-bottom: 20px;
+}
+</style>
