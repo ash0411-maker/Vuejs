5ac51f5dc0d2f262100d6d1cbb25ef83d66a8059
ash <satoshi.0411@icloud.com>
2021-04-18 11:46:07 +0900

    v-model

---
+++ Udemy/section9/App_vue/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h2>イベントのフォーム</h2>
+    <label for="title">タイトルフォーム</label>
+    <input
+      type="text"
+      id="title"
+      v-model.lazy="eventData.title"
+    >
+    <p>インプット文字: {{eventData.title}}</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  data() {
+    return {
+      number: 10,
+      eventData: {
+        title: ""
+      }
+    }
+  }
+}
+</script>
