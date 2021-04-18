12888c471c800f5b6623af95d98911155e7bf28f
ash <satoshi.0411@icloud.com>
2021-04-18 12:20:53 +0900

    v-model components

--- Udemy/section9/App_vue/src/App.vue
+++ Udemy/section9/App_vue/src/App.vue
@@ -1,13 +1,8 @@
 <template>
   <div>
     <h2>イベントのフォーム</h2>
-    <label for="title">タイトルフォーム</label>
-    <input
-      type="text"
-      id="title"
-      v-model.lazy="eventData.title"
-    >
-    <p>インプット文字: {{eventData.title}}</p>
+    <EventTitle v-model="eventData.title"></EventTitle>
+
 
     <label for="title">最大人数</label>
     <input
@@ -15,22 +10,44 @@
       id="maxNumber"
       v-model.number="eventData.maxNumber"
     >
-    <p>インプット文字: {{ eventData.maxNumber} }</p>
+    <p>インプット文字: {{ eventData.maxNumber}}</p>
+
+    <label for="title">最大人数</label>
+    <input
+      type="text"
+      id="host"
+      v-model.trim="eventData.host"
+    >
+    <p>インプット文字: {{ eventData.host}}</p>
+
+    <label for="detail">イベントの内容</label>
+    <textarea
+      id="detail"
+      cols="30"
+      rows="10"
+      v-model="eventData.detail"
+    >
+    </textarea>
+    <pre>インプット文字: {{ eventData.detail}}</pre>
   </div>
 </template>
 
 <script>
-
+import EventTitle from "./components/EventTitle.vue"
 
 export default {
   data() {
     return {
-      number: 10,
       eventData: {
         title: "",
-        maxNumber: 0
+        maxNumber: 0,
+        host: "",
+        detail: ""
       }
     }
+  },
+  components: {
+    EventTitle,
   }
 }
 </script>
