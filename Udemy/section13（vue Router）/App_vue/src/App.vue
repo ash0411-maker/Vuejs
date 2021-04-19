1c474059b5ebb3ed07bfe3be40eba7d46e009fcc
ash <satoshi.0411@icloud.com>
2021-04-19 21:14:58 +0900

    js vue-router

--- Udemy/section13（vue Router）/App_vue/src/App.vue
+++ Udemy/section13（vue Router）/App_vue/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div style="width: 700px; margin: auto; padding-top: 70px;">
     <nav>
-      <router-link to="/">HOME</router-link>
-      <router-link to="/users">USERS</router-link>
+      <router-link
+        to="/"
+        active-class="link--active"
+        exact
+        class="link"
+      >
+      HOME
+      </router-link>
+      <router-link
+        to="/users"
+        active-class="link--active"
+        exact
+        class="link"
+      >
+      USERS
+      </router-link>
     </nav>
     <router-view></router-view>
   </div>
@@ -16,3 +30,14 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+  .link {
+    margin-right: 20px;
+    background-color: limegreen;
+  }
+
+  .link--active {
+    font-size: 20px;
+  }
+</style>
