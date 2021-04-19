81bddec6f0611e8aa6f496de0f34305b7958bf4b
ash <satoshi.0411@icloud.com>
2021-04-19 21:02:57 +0900

    router-link

--- Udemy/section13（vue Router）/App_vue/src/App.vue
+++ Udemy/section13（vue Router）/App_vue/src/App.vue
@@ -1,5 +1,9 @@
 <template>
-  <div>
+  <div style="width: 700px; margin: auto; padding-top: 70px;">
+    <nav>
+      <router-link to="/">HOME</router-link>
+      <router-link to="/users">USERS</router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
